c3f14a00e2c12724255a5db1022a08f9e6450e3b
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-27 16:28:06 +0300

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 """a function named index_range that takes two integer arguments page and page_size"""
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page, page_size):
     """return a tuple of size two containing a start index and an end index"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
