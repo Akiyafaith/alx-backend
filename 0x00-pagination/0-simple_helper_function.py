a4965c168ed39b3fda9fe4c3c980e461f75a4817
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-27 16:33:59 +0300

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""a function named index_range that takes two integer arguments page and page_size"""
+"""a function named index_range that takes two
+integer arguments page and page_size"""
 
 
 def index_range(page, page_size):
