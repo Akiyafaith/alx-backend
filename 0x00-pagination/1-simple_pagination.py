8424e0ea3f746608ca610545cb4b33606ad005e9
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-27 21:35:30 +0300

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -2,7 +2,6 @@
 """pagination """
 import csv
 from typing import List, Tuple
-from .simple_helper_function import index_range
 
 
 class Server:
@@ -33,3 +32,8 @@ class Server:
 
         dataset = self.dataset()
         return dataset[start_index:end_index]
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """Calculate the start and end index for a specified page and page size."""
+        start_index = (page - 1) * page_size
+        end_index = page * page_size
+        return (start_index, end_index)
