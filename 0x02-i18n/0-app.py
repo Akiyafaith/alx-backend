5a65bf0c802bd07ee19e31bfb91db39d1a480b6b
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-31 13:49:35 +0300

    flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,10 +4,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
     """render the template"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
